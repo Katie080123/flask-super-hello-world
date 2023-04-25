099b5302d78bdf3dd36af3f19efb349bc45b3eb8
katie <katiesilva.2000@gmail.com>
2023-04-25 14:12:04 -0400

    trabajo final

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html', user = request.args.get('username'))
